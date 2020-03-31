1efa069499478affe92b980005d5739f43d24724
Tamires <tamiresmanhaes4@gmail.com>
2020-03-31 11:45:21 -0300

    iniciando módulo Node

---
+++ Node/moduloA.js
@@ -0,0 +1,6 @@
+// em Node um arquivo representa um módulo 
+this.ola = 'Fala galeuren!';
+
+exports.bemVindo = 'bem vindo ao meu mundo!';
+
+module.exports.ateMais = 'até a proxima aula!';
---
+++ Node/moduloCliente.js
@@ -0,0 +1,9 @@
+const moduloA = require('./moduloA');
+const moduloB = require('./moduloB');
+
+console.log(moduloA.ola);
+console.log(moduloB.boaNoite());
+console.log('modulo A');
+console.log(moduloA);
+console.log('modulo B');
+console.log(moduloB);
