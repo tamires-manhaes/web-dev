3cbcd888efa2109c7da9bb0b2ff64a5df0dc58e4
Tamires <tamiresmanhaes4@gmail.com>
2020-04-03 14:00:43 -0300

    desafio com filter e reduce

--- 08 Node/funcionarios/funcionarios.js
+++ 08 Node/funcionarios/funcionarios.js
@@ -1,8 +1,11 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 const axios = require('axios');
 
+// filtro para encontrar apenas chineses
 const chineses = f => f.pais === 'China';
+// filtro para encontrar apenas mulheres 
 const mulheres = f => f.genero === 'F';
+// filtro para buscar o menor salário
 const menorSalario = (func, funcAtual) => {
     return func.salario < funcAtual.salario ? func : funcAtual;
 }
